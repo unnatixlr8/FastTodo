96c302015e6f711916946a32774090177e955878
Unnati Gupta <unnatixlr8@gmail.com>
2023-02-26 03:12:45 +0530

    added routes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
@@ -6,6 +7,29 @@ app = FastAPI()
 def index():
     return {'author': {'name': 'Unnati'}}
 
+
 @app.get('/about')
 def about():
     return {'data': 'This app is made using FastAPI and Python by Unnati Gupta'}
+
+
+@app.get('/todo')
+def todo(limit = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published from the db'}
+    else:
+        return {'data': f'{limit} unpublished todos from the db'}
+    
+
+@app.get('/todo/draft')
+def draft():
+    return {'data':'unpublished'}
+
+
+@app.get('/todo/{id}')
+def show(id: int):
+    return {'todo': id}
+
+@app.post('/todo')
+def create_todo():
+    return {'data': 'Post is created'}
